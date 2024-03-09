ab6c593eb00691c8c49c93c422b01970222faab6
arfanazmol <arfanazmol66@gmail.com>
2024-03-10 01:07:57 +0600

    fetch basic practice

--- fetch.js
+++ fetch.js
@@ -23,3 +23,27 @@ const keys = Object.keys(student);
 const values = Object.values(student);
 
 //for
+const numbers = [23, 45, 78, 46, 3, 34]
+numbers.forEach(num => console.log(num));
+numbers.map(num => num * 2);
+
+// for of on array like object
+// loop on an object using for in
+
+// add or remove from an array
+const products = [
+  { name: 'laptop', price: 3200, brand: 'len', color: 'white' },
+  { name: 'phone', price: 120000, brand: 'HTC', color: 'yellow' },
+  { name: 'sunglass', price: 300, brand: 'ribon', color: 'black' },
+  { name: 'camera', price: 10000, brand: 'canon', color: 'gray' },
+  { name: 'wacth', price: 3000, brand: 'casio', color: 'silver' },
+];
+
+const newProduct = { name: 'webcam', price: 700, brand: 'lal' }
+
+//copy products array and then add newproduct
+const newProducts = [...products, newProduct];
+
+// cerate a new array without one specefic item
+// remove phone means create a new array without the phone
+const remaining = product.filter(p => p.name !== 'phone');
