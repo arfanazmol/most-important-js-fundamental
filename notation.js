6315e374427bb5aecfc4df7d42e59517f59dd66a
arfanazmol <arfanazmol66@gmail.com>
2024-03-13 13:02:56 +0600

    notation practice

---
+++ notation.js
@@ -0,0 +1,21 @@
+const person = {
+  name: 'Lal Nil Holud Mia',
+  profession: 'Traffic Surgeon',
+  age: 39,
+  25: 'summer',
+  'son-name': 'sunny',
+  address: 'Shahabag'
+}
+
+// dot notation
+const prof1 = person.profession;
+
+//bracket notation
+const prof2 = person['profession'];
+const pName = 'proffession';
+const prof3 = person[pName];
+// console.log(prof3)
+
+const seson = person[25];
+// console.log(seson)
+const son = person['son-name'];
